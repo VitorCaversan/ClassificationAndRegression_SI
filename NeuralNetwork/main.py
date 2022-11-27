38cf1ef8fca396d3ee56e1f20f8e140cff6b6178
VitorCaversan <joaopassos@alunos.utfpr.edu.br>
2022-11-26 23:48:03 -0300

    NN fully functional. Tweaking of parameters is needed.

--- NeuralNetwork/main.py
+++ NeuralNetwork/main.py
@@ -1,10 +1,29 @@
 from neuralNetwork import NeuralNet
 
 def main():
+    print("Criar (0) ou carregar (1) modelo?: ", end='')
+    decision = int(input())
     neuralNet = NeuralNet('../tar2_sinais_vitais_treino_com_label.txt')
+    isOk = True
 
-    neuralNet.createModel()
-    neuralNet.runModel()
+    if decision == 0:
+        neuralNet.createModel()
+        neuralNet.runModel()
+    else:
+        isOk = neuralNet.loadModel()
+
+    if isOk:
+        rowQnt = 1
+        while rowQnt != 0:
+            print("How many rows do you want to predict?: ", end='')
+            rowQnt = int(input())
+            if rowQnt != 0:
+                neuralNet.predictData(rowQnt)
+
+        print("Do you want to save this model? (1 yes, 0 no): ", end='')
+        save = int(input())
+        if save == 1:
+            neuralNet.saveModel()
 
     return
 
--- NeuralNetwork/neuralNetwork.py
+++ NeuralNetwork/neuralNetwork.py
@@ -16,37 +16,39 @@ class NeuralNet:
         # pandas.options.display.max_columns = 50
         # self.fileData = pandas.read_csv(csvFilePath, encoding = 'utf-8')
         self.fileData = self.manualParsing(csvFilePath)
+        self.colNames = []
+        self.input    = []
+        self.output   = []
 
         return
 
     def createModel(self):
-        self.fileData = self.fileData[1:]
-        shuffledData = shuffle(self.fileData)
+        self.colNames = self.fileData[0][3:-1]
+        shuffledData  = shuffle(self.fileData[1:])
+        # Used with pandas:
         # self.input  = self.fileData[['pSist', 'pDiast', 'qPA', 'pulso', 'resp', 'grav']] # NN samples
         # self.output = self.fileData['risco'].values # NN labels
 
-        self.input = []
-        self.output = []
-
-        lastRow = len(self.fileData[0]) - 1
-        for row in self.fileData:
-            self.input.append(row[1:-1])
-            self.output.append(row[lastRow])
+        gravRow = len(self.fileData[0]) - 2
+        for row in shuffledData:
+            self.input.append(row[3:-2])
+            self.output.append(row[gravRow])
 
         self.input = numpy.array(self.input)
-        self.input = normalize(self.input, axis=1) # Must normalize to work
+        # self.input = normalize(self.input) # Normalize when values have a great range
 
         self.output = numpy.array(self.output)
-        self.output = to_categorical(self.output) # An encoding to classify each label with only one bit
+        # An encoding to classify each label with only one bit. Used for multiclass:
+        # self.output = to_categorical(self.output, num_classes=4, dtype=int)
 
+        print(self.colNames)
         print(self.input)
         print(self.output)
-        # scaler = MinMaxScaler(feature_range=(0,1))
-        # self.input = scaler.fit_transform(self.input.reshape(-1,1))
 
-        self.model = Sequential([Dense(units=16, input_shape=(6,), activation='relu'),
+        self.model = Sequential([Dense(units=128, input_dim=3, activation='relu'),
+                                 Dense(units=64, activation='relu'),
                                  Dense(units=32, activation='relu'),
-                                 Dense(units=5, activation='softmax')]) # Softmax always recomended for multiclass problems
+                                 Dense(units=1, activation='linear')]) # 'softmax' for multiclass problems
 
         self.model.summary()
 
@@ -54,15 +56,52 @@ class NeuralNet:
     
     def runModel(self):
         self.model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001), # Or rmsprop
-                           loss='categorical_crossentropy', # Also recomended for multiclass
-                           metrics=[tf.keras.metrics.Accuracy(name="accuracy"),
-                                    tf.keras.metrics.Recall(name="recall")])
+                           loss='mean_squared_error', # categorical_crossentropy for multiclass
+                           metrics=['mae'])
+                                    # Other possible metrics:
+                                    # 'accuracy',
+                                    # tf.keras.metrics.Accuracy(name="accuracy"),
+                                    # tf.keras.metrics.Recall(name="recall")])
                                     # tf.keras.metrics.TruePositives(name="truePositives"),
                                     # tf.keras.metrics.TrueNegatives(name="trueNegatives"),
                                     # tf.keras.metrics.FalsePositives(name="falsePositives"),
                                     # tf.keras.metrics.FalseNegatives(name="falseNegatives")])
 
-        self.model.fit(x=self.input, y=self.output, steps_per_epoch=3 ,validation_split=0.05, batch_size=20, epochs=100, shuffle=True, verbose=1)
+        self.model.fit(x=self.input,
+                       y=self.output,
+                       # steps_per_epoch=3, # completely fucks up the learning process
+                       validation_split=0.1,
+                       # batch_size=20,
+                       epochs=300,
+                       shuffle=True,
+                       verbose=1)
+
+        return
+
+    def predictData(self, rowsQntToPredict: int) -> None:
+        predictions = self.model.predict(self.input[:rowsQntToPredict])
+
+        print("Predicted values are: \n", predictions)
+        print("Real values are: \n", self.output[:rowsQntToPredict])
+        print("")
+
+        return
+
+    def saveModel(self):
+        self.model.save('models/sinais_vitais_model.h5')
+        return
+
+    def loadModel(self) -> bool:
+        try:
+            self.model  = load_model('models/sinais_vitais_model.h5')
+            gravRow = len(self.fileData[0]) - 2
+            for row in self.fileData[1:]:
+                self.input.append(row[3:-2])
+                self.output.append(row[gravRow])
+            return True
+        except:
+            print("file was not found \n")
+            return False
 
     def manualParsing(self, filePath):
         fileContent = open(filePath, 'r')
