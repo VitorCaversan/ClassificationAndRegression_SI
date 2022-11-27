c4c07c9ce81855ff6514573ca28d308c8543dd6f
VitorCaversan <joaopassos@alunos.utfpr.edu.br>
2022-11-27 00:03:37 -0300

    Makes user interface better.

--- NeuralNetwork/main.py
+++ NeuralNetwork/main.py
@@ -1,12 +1,12 @@
 from neuralNetwork import NeuralNet
 
 def main():
-    print("Criar (0) ou carregar (1) modelo?: ", end='')
+    print("\nCreate (1) or load (0) model?: ", end='')
     decision = int(input())
     neuralNet = NeuralNet('../tar2_sinais_vitais_treino_com_label.txt')
     isOk = True
 
-    if decision == 0:
+    if decision == 1:
         neuralNet.createModel()
         neuralNet.runModel()
     else:
@@ -15,12 +15,12 @@ def main():
     if isOk:
         rowQnt = 1
         while rowQnt != 0:
-            print("How many rows do you want to predict?: ", end='')
+            print("\nHow many rows do you want to predict?: ", end='')
             rowQnt = int(input())
             if rowQnt != 0:
                 neuralNet.predictData(rowQnt)
 
-        print("Do you want to save this model? (1 yes, 0 no): ", end='')
+        print("\nDo you want to save this model? (1 yes, 0 no): ", end='')
         save = int(input())
         if save == 1:
             neuralNet.saveModel()
